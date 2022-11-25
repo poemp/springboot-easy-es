4affe93835fcb5d54fc2afc72a9d7f1d9a2f986b
poem <xue_2013@sina.com>
2022-11-25 14:58:30 +0800

    init

---
+++ src/main/java/org/poem/springbooteasyes/SpringbootEasyEsApplication.java
@@ -0,0 +1,13 @@
+package org.poem.springbooteasyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEasyEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEasyEsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/poem/springbooteasyes/SpringbootEasyEsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poem.springbooteasyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEasyEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
